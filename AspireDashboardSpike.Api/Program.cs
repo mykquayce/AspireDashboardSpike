f014e552fb4b3c5f6e84edc45d3363ff6e97b1cc
bob h <mail@bob.codes>
2024-05-09 23:17:13 +0100

    adding code from video
    https://www.youtube.com/watch?v=617oVraGY_M

--- AspireDashboardSpike.Api/Program.cs
+++ AspireDashboardSpike.Api/Program.cs
@@ -1,11 +1,68 @@
+using AspireDashboardSpike.Api;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddOpenTelemetry(x =>
+{
+	x.IncludeScopes = true;
+	x.IncludeFormattedMessage = true;
+});
+
+builder.Services.AddOpenTelemetry()
+	.WithMetrics(x =>
+	{
+		x.AddRuntimeInstrumentation()
+			.AddMeter(
+				"Microsoft.AspNetCore.Hosting",
+				"Microsoft.AspNetCore.Server.Kestrel",
+				"System.Net.Http",
+				"AspireDashboardSpike.Api");
+	})
+	.WithTracing(x =>
+	{
+		if (builder.Environment.IsDevelopment())
+		{
+			x.SetSampler<AlwaysOnSampler>();
+		};
+
+		x.AddAspNetCoreInstrumentation()
+			.AddGrpcClientInstrumentation()
+			.AddHttpClientInstrumentation();
+	});
+
+builder.Services.Configure<OpenTelemetryLoggerOptions>(logging => logging.AddOtlpExporter());
+builder.Services.ConfigureOpenTelemetryMeterProvider(metrics => metrics.AddOtlpExporter());
+builder.Services.ConfigureOpenTelemetryTracerProvider(tracing => tracing.AddOtlpExporter());
+
+builder.Services.AddHealthChecks()
+	.AddCheck("self", () => HealthCheckResult.Healthy(), ["live",]);
+
+builder.Services.ConfigureHttpClientDefaults(http =>
+{
+	http.AddStandardResilienceHandler();
+});
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddMetrics();
+builder.Services.AddSingleton<WeatherMetrics>();
+
 var app = builder.Build();
+
+app.MapHealthChecks("/health");
+
+app.MapHealthChecks("/alive", new HealthCheckOptions
+{
+	Predicate = r => r.Tags.Contains("live"),
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -19,17 +76,27 @@ var summaries = new[]
 	"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/weatherforecast", async (WeatherMetrics weatherMetrics) =>
 {
-	var forecast = Enumerable.Range(1, 5).Select(index =>
-		new WeatherForecast
-		(
-			DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-			Random.Shared.Next(-20, 55),
-			summaries[Random.Shared.Next(summaries.Length)]
-		))
-		.ToArray();
-	return forecast;
+	using var _ = weatherMetrics.MeasureRequestDuration();
+	try
+	{
+		await Task.Delay(Random.Shared.Next(5, 100));
+		var forecast = Enumerable.Range(1, 5).Select(index =>
+			new WeatherForecast
+			(
+				DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+				Random.Shared.Next(-20, 55),
+				summaries[Random.Shared.Next(summaries.Length)]
+			))
+			.ToArray();
+		return forecast;
+	}
+	finally
+	{
+		weatherMetrics.IncreaseWeatherRequestCount();
+	}
+
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
---
+++ AspireDashboardSpike.Api/WeatherMetrics.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace AspireDashboardSpike.Api;
+
+public class WeatherMetrics
+{
+	public const string MeterName = "AspireDashboardSpike.Api";
+
+	private readonly Counter<long> _weatherRequestCounter;
+	private readonly Histogram<double> _weatherRequestDuration;
+
+	public WeatherMetrics(IMeterFactory meterFactory)
+	{
+		var meter = meterFactory.Create(MeterName);
+
+		_weatherRequestCounter = meter.CreateCounter<long>(
+			"aspiredashboardspike.api.weather_requests.count");
+
+		_weatherRequestDuration = meter.CreateHistogram<double>(
+			"aspiredashboardspike.api.weather_requests.duration",
+			unit: "ms");
+	}
+
+	public void IncreaseWeatherRequestCount() => _weatherRequestCounter.Add(1);
+	public TrackedRequestDuration MeasureRequestDuration() => new(_weatherRequestDuration);
+}
+
+public sealed class TrackedRequestDuration(Histogram<double> histogram) : IDisposable
+{
+	private readonly long _requestStartTime = TimeProvider.System.GetTimestamp();
+
+	public void Dispose()
+	{
+		var elapsed = TimeProvider.System.GetElapsedTime(_requestStartTime);
+		histogram.Record(elapsed.TotalMilliseconds);
+	}
+}
